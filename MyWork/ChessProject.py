cd915111da1bc9d1d6bcf18a1993f979e91f5bb9
CurtisCanby <canbycurtis@gmail.com>
2025-02-27 11:12:37 -0500

    projects

--- MyWork/ChessProject.py
+++ MyWork/ChessProject.py
@@ -1,27 +1,104 @@
 import chess
-import chess.engine
-import random
 
-engine = chess.engine.SimpleEngine.popen_uci("PATH TO STOCKFISH") # I NEED TO GET THE STOCKFISH ENGINE AND PUT THE PATH HERE
-games = []
+def evaluate_board(board):
+  piece_values = {
+        chess.PAWN: 1,
+        chess.KNIGHT: 3,
+        chess.BISHOP: 3,
+        chess.ROOK: 5,
+        chess.QUEEN: 9,
+        chess.KING: 0
+    }
+  score = 0
+  for square, piece in board.piece_map().items():
+    if piece.color == chess.WHITE:
+        score += piece_values[piece.piece_type]
+    else:
+        score -= piece_values[piece.piece_type]
+  return score
 
-def generate_game(engine, min_elo, max_elo, max_moves):
-  board = chess.Board()
-  engine.configure({"Skill Level": random.randint(min_elo, max_elo)})
+def minimax(board, depth, maximizing_player):
+    if depth == 0 or board.is_game_over():
+      return evaluate_board(board)
+    if maximizing_player:
+      max_eval = -float("inf")
+      for move in board.legal_moves:
+        board.push(move)
+        eval = minimax(board, depth - 1, False)
+        board.pop()
+        max_eval = max(max_eval, eval)
+      return max_eval
+    else:
+      min_eval = float("inf")
+      for move in board.legal_moves:
+        board.push(move)
+        eval = minimax(board, depth - 1, True)
+        board.pop()
+        min_eval = min(min_eval, eval)
+      return min_eval
 
-  moves = []
-  while not board.is_game_over() and len(moves) < max_moves:
-    result = engine.play(board, chess.engine.Limit(time=0.1))
-    moves.append(result.move)
-    board.push(result.move)
+def minimax_alpha_beta(board, depth, alpha, beta, maximizing_player):
+    if depth == 0 or board.is_game_over():
+        return evaluate_board(board)
+    if maximizing_player:
+        max_eval = -float("inf")
+        for move in board.legal_moves:
+            board.push(move)
+            eval = minimax_alpha_beta(board, depth - 1, alpha, beta, False)
+            board.pop()
+            max_eval = max(max_eval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return max_eval
+    else:
+        min_eval = float("inf")
+        for move in board.legal_moves:
+            board.push(move)
+            eval = minimax_alpha_beta(board, depth - 1, alpha, beta, True)
+            board.pop()
+            min_eval = min(min_eval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return min_eval
 
-  return moves
+def getbestmove(board, depth):
+  best_move = None
+  if board.turn == chess.WHITE:
+      best_value = -float("inf")
+      for move in board.legal_moves:
+          board.push(move)
+          board_value = minimax_alpha_beta(board, depth - 1, -float("inf"), float("inf"), False)
+          board.pop()
+          if board_value > best_value:
+              best_value = board_value
+              best_move = move
+  else:
+      best_value = float("inf")
+      for move in board.legal_moves:
+          board.push(move)
+          board_value = minimax_alpha_beta(board, depth - 1, -float("inf"), float("inf"), True)
+          board.pop()
+          if board_value < best_value:
+              best_value = board_value
+              best_move = move
+  return best_move
 
-for i in range(10000):
-  moves = generate_game(engine, 1000, 2000, 150) 
-  games.append(moves)
+def playgame(board,depth):
+  while not board.is_game_over():
+    if board.turn == chess.WHITE:
+      move = input("Enter your move (e.g. e2e4): ")
+      board.push_uci(move)
+      print(board)
+      print("---")
+    else:
+      botmove = getbestmove(board,depth)
+      board.push(botmove)
+      print("The Bot played " + str(botmove))
+      print(board)
+      print("---")
+  print("Game Over!")  
 
-engine.quit()
-# NOW THERE IS A GAMES LIST THAT IS FULL OF DATA!!!
-# WITH THIS DATA WE WANT TO TRAIN AN ENGINE
-# BUT AI CANT READ CHESS FEN NOTATION. ENCODE ALL THE MOVES INTO UNIQUE INTEGER IDs!
+board = chess.Board()
+playgame(board,4)
